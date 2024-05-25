67b30172e7eed7efacbc2649832c40a0ab0f1628
minhnhatmn16 <minhnhatmn16@gmail.com>
2024-05-25 17:13:47 +0700

    Recognize_Faces ( Connect Firebase )

--- Recognize_Faces/Test.py
+++ Recognize_Faces/Test.py
@@ -51,17 +51,25 @@
 #
 # cv2.destroyAllWindows()
 
+# url = 'http://192.168.0.7/cam-lo.jpg'
+# url = 'http://192.168.111.172/cam-lo.jpg'
 url = 'http://192.168.0.8/cam-lo.jpg'
+
 import urllib.request
 import cv2
 import numpy as np
 import os
+import firebase_admin
+from firebase_admin import db, credentials
 
 recognizer = cv2.face.LBPHFaceRecognizer.create()
 recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
+cred = credentials.Certificate('serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {"databaseURL" : "https://smarthome-6ad5a-default-rtdb.firebaseio.com/"})
+ref = db.reference("/")
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 id = 0
@@ -72,8 +80,8 @@ names = ['Minh Nhat','Duy Tin','Anh Quan','Ngoc Anh']
 # cam.set(3, 640)
 # cam.set(4, 480)
 #
-minW = 0.1 * 640
-minH = 0.1 * 480
+minW = 0.1 * 1024
+minH = 0.1 * 768
 
 while True:
     # ret, img = cam.read()
@@ -95,7 +103,9 @@ while True:
         id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
         if (confidence < 100):
+
             id = names[id]
+            ref.update({"door" : 1})
         else:
             id = "Unknown"
 
@@ -104,6 +114,7 @@ while True:
         cv2.putText(img, str(id), (x + 10, y), font, 1, (0, 0, 255), 2)
         # cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
+    # img = cv2.resize(img, (640,480))
     cv2.imshow('Nhan dien khuon mat', img)
 
     k = cv2.waitKey(10) & 0xff
